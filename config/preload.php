162f59fc1e31f9edabf4361aa7b613eca2077b21
fvalverde <franwalverde@me.com>
2023-09-14 23:36:08 +0200

    feat(lint): code sniffer rules

--- config/preload.php
+++ config/preload.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 if (file_exists(dirname(__DIR__).'/var/cache/prod/Whalar_KernelProdContainer.preload.app')) {
     require dirname(__DIR__).'/var/cache/prod/Whalar_KernelProdContainer.preload.app';
 }
